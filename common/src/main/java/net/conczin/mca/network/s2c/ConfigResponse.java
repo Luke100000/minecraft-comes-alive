66262914e6f3ce3b1194ecd7ac0f4e5eaaeced30
Mik <29287456+Mikulasz12@users.noreply.github.com>
2026-07-17 15:42:58 +0100

    feat: sync destiny locations and tracked data

--- common/src/main/java/net/conczin/mca/network/s2c/ConfigResponse.java
+++ common/src/main/java/net/conczin/mca/network/s2c/ConfigResponse.java
@@ -1,6 +1,7 @@
 package net.conczin.mca.network.s2c;
 
 import com.google.gson.Gson;
+import com.google.gson.JsonObject;
 import net.conczin.mca.ClientProxy;
 import net.conczin.mca.CommonConfig;
 import net.conczin.mca.Config;
@@ -11,6 +12,8 @@ import net.minecraft.network.codec.ByteBufCodecs;
 import net.minecraft.network.codec.StreamCodec;
 import net.minecraft.network.protocol.common.custom.CustomPacketPayload;
 import net.minecraft.world.entity.player.Player;
+
+import java.util.List;
 
 public record ConfigResponse(String json) implements HandleablePayload {
     public static final CustomPacketPayload.Type<ConfigResponse> TYPE = new CustomPacketPayload.Type<>(MCA.locate("config_response"));
@@ -23,6 +26,16 @@ public record ConfigResponse(String json) implements HandleablePayload {
 
     public ConfigResponse(Config config) {
         this(GSON.toJson(config, CommonConfig.class));
+    }
+
+    public ConfigResponse(Config config, List<String> destinySpawnLocations) {
+        this(createJson(config, destinySpawnLocations));
+    }
+
+    private static String createJson(Config config, List<String> destinySpawnLocations) {
+        JsonObject json = GSON.toJsonTree(config, CommonConfig.class).getAsJsonObject();
+        json.add("destinySpawnLocations", GSON.toJsonTree(destinySpawnLocations));
+        return GSON.toJson(json);
     }
 
     public CommonConfig getConfig() {
