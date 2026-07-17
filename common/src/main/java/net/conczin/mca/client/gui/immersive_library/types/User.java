0dea7c9f8803653f1bf1c20a91c31f6ba2b526c3
Luke100000 <jonas.peche@aon.at>
2026-07-17 23:03:02 +0200

    submission and likes paging

--- common/src/main/java/net/conczin/mca/client/gui/immersive_library/types/User.java
+++ common/src/main/java/net/conczin/mca/client/gui/immersive_library/types/User.java
@@ -1,16 +1,5 @@
 package net.conczin.mca.client.gui.immersive_library.types;
 
-import java.util.LinkedList;
-import java.util.List;
-
-public record User(int userid, String username, int likes_received, List<LiteContent> likes,
-                   List<LiteContent> submissions, boolean moderator) {
-    public User(int userid, String username, int likes_received, List<LiteContent> likes, List<LiteContent> submissions, boolean moderator) {
-        this.userid = userid;
-        this.username = username;
-        this.likes_received = likes_received;
-        this.likes = new LinkedList<>(likes);
-        this.submissions = new LinkedList<>(submissions);
-        this.moderator = moderator;
-    }
+public record User(int userid, String username, int submission_count, int likes_given, int likes_received,
+                   boolean moderator) {
 }
